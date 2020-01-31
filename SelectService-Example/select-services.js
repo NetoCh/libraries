5406fa1bce2b1becec660c77307622081206322d
NetoCh <54650291+NetoCh@users.noreply.github.com>
2020-01-30 20:56:00 -0500

    v1.1
    Using an object to configure options properties

--- SelectService-Example/select-services.js
+++ SelectService-Example/select-services.js
@@ -6,35 +6,29 @@ function SelectServices() {
     if (typeof arguments[arguments.length - 1] === "function") {
       fn = arguments[arguments.length - 1];
     }
-
     let result = {
       success: true,
       message: ""
     };
-
     if (arguments[0].length > 0) {
       let arrayList = arguments[0];
       let elementId = arguments[1];
-      let parameter1 = arguments[2];
-      let parameter2 = arguments[3];
-      arrayList.forEach(function(item) {
-        let opt = document.createElement("option");
-        if (parameter1 !== undefined && parameter1 != fn) {
-          opt.text = item[parameter1];
-        } else {
-          opt.text = item;
-        }
-        if (parameter2 !== undefined && parameter2 != fn) {
-          opt.value = item[parameter2];
-        }
-        document.getElementById(elementId).appendChild(opt);
-      });
-      result.message = "Success";
+      let properties = arguments[2];
+      if (typeof properties === "object" && properties !== fn) {
+        arrayList.forEach(function(item) {
+          let opt = document.createElement("option");
+          let option = self.createElement(item, opt, properties);
+          document.getElementById(elementId).appendChild(option);
+        });
+        result.message = "Success";
+      } else {
+        result.success = false;
+        result.message = "Third parameter needs to be an object";
+      }
     } else {
       result.success = true;
       result.message = "Empty Array";
     }
-
     if (fn !== undefined && typeof fn === "function") {
       fn(result);
     }
@@ -49,30 +43,26 @@ function SelectServices() {
       success: true,
       message: ""
     };
-    if (arguments[0].length > 0) {
-      let arrayList = arguments[0];
-      let elementId = arguments[1];
-      let parameter1 = arguments[2];
-      let parameter2 = arguments[3];
-      clearAll(elementId);
-      arrayList.forEach(function(item) {
-        let opt = document.createElement("option");
-        if (parameter1 !== undefined && parameter1 != fn) {
-          opt.text = item[parameter1];
-        } else {
-          opt.text = item;
-        }
-        if (parameter2 !== undefined && parameter2 != fn) {
-          opt.value = item[parameter2];
-        }
-        document.getElementById(elementId).appendChild(opt);
-      });
-      result.message = "Success";
+    if (typeof properties === "object" && properties !== fn) {
+      if (arguments[0].length > 0) {
+        let arrayList = arguments[0];
+        let elementId = arguments[1];
+        let properties = arguments[2];
+        self.clearAll(elementId);
+        arrayList.forEach(function(item) {
+          let opt = document.createElement("option");
+          let option = self.createElement(item, opt, properties);
+          document.getElementById(elementId).appendChild(option);
+        });
+        result.message = "Success";
+      } else {
+        result.success = true;
+        result.message = "Empty Array";
+      }
     } else {
-      result.success = true;
-      result.message = "Empty Array";
+      result.success = false;
+      result.message = "Third parameter needs to be an object";
     }
-
     if (fn !== undefined && typeof fn === "function") {
       fn(result);
     }
@@ -87,41 +77,37 @@ function SelectServices() {
       success: true,
       message: ""
     };
-    if (arguments[0].length > 0) {
-      let arrayList = arguments[0];
-      let elementId = arguments[1];
-      let parameter1 = arguments[2];
-      let parameter2 = arguments[3];
-      let startingPoint = arguments[4];
-      let select = document.getElementById(elementId);
-      let selectLength = select.options.length;
-      if (startingPoint < selectLength) {
-        let amountToPop = selectLength - startingPoint;
-        for (let i = 0; i < amountToPop; i++) {
-          select.remove(startingPoint);
+    if (typeof properties === "object" && properties !== fn) {
+      if (arguments[0].length > 0) {
+        let arrayList = arguments[0];
+        let elementId = arguments[1];
+        let properties = arguments[2];
+        let startingPoint = arguments[3];
+        let select = document.getElementById(elementId);
+        let selectLength = select.options.length;
+        if (startingPoint < selectLength) {
+          let amountToPop = selectLength - startingPoint;
+          for (let i = 0; i < amountToPop; i++) {
+            select.remove(startingPoint);
+          }
+          arrayList.forEach(function(item) {
+            let opt = document.createElement("option");
+            let option = self.createElement(item, opt, properties);
+            document.getElementById(elementId).appendChild(option);
+          });
+          result.message = "Success";
+        } else {
+          result.success = false;
+          result.message = "Theres no enough items in the select";
         }
-        arrayList.forEach(function(item) {
-          let opt = document.createElement("option");
-          if (parameter1 !== undefined && parameter1 != fn) {
-            opt.text = item[parameter1];
-          } else {
-            opt.text = item;
-          }
-          if (parameter2 !== undefined && typeof parameter2 != fn) {
-            opt.value = item[parameter2];
-          }
-          document.getElementById(elementId).appendChild(opt);
-        });
-        result.message = "Success";
       } else {
-        result.success = false;
-        result.message = "Theres no enough items in the select";
+        result.success = true;
+        result.message = "Empty Array";
       }
     } else {
-      result.success = true;
-      result.message = "Empty Array";
+      result.success = false;
+      result.message = "Third parameter needs to be an object";
     }
-
     if (fn !== undefined && typeof fn === "function") {
       fn(result);
     }
@@ -131,5 +117,45 @@ function SelectServices() {
     let select = document.getElementById(elementId);
     select.options.length = 0;
   };
+
+  this.createElement = function(item, opt, properties) {
+    if (
+      !(
+        Object.keys(properties).length === 0 &&
+        properties.constructor === Object
+      )
+    ) {
+      if (typeof item === "string") {
+        opt.text = item;
+      } else {
+        if (properties["id"] !== undefined && properties["id"] !== "")
+          opt.id = item[properties["id"]];
+        if (properties["text"] !== undefined && properties["text"] !== "")
+          opt.text = item[properties["text"]];
+        if (properties["value"] !== undefined && properties["value"] !== "")
+          opt.value = item[properties["value"]];
+        if (properties["title"] !== undefined && properties["title"] !== "")
+          opt.title = item[properties["title"]] || properties["title"];
+        if (properties["label"] !== undefined && properties["label"] !== "")
+          opt.label = item[properties["label"]] || properties["label"];
+        if (
+          properties["disabled"] !== undefined &&
+          properties["disabled"] !== ""
+        )
+          opt.disabled =
+            item[properties["disabled"]] !== undefined
+              ? !item[properties["disabled"]]
+              : properties["disabled"];
+        if (properties["class"] !== undefined && properties["class"] !== "")
+          item[properties["class"]] !== undefined
+            ? opt.classList.add(item[properties["class"]])
+            : opt.classList.add(properties["class"]);
+      }
+    } else {
+      opt.text = item;
+    }
+
+    return opt;
+  };
 }
-const selector = new SelectServices();
+const SELECT = new SelectServices();
